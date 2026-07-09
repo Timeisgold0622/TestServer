e2937fcad67c43ef543d8d39db79f2e029746fd7
Timeisgold0622 <yjok9333@gmail.com>
2026-07-10 08:58:03 +0900

    Interlocked를 이용한 원자적 연산 배움

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -2,55 +2,33 @@
 {
     class Program
     {
-        // 메모리 배리어
-        // A) 코드 재배치 억제
-        // B) 가시성( 동기화 작업 )
-
-        // 1) Full Memory Barrier (ASM MFENCE, C# Thread.MemoryBarrier) : Store / Load 둘다 막는다
-        // 2) Store Memory Barrier (ASM SFENCE) : Store만 막는다
-        // 3) Load Memory Barrier (ASM LFENCE) : Load만 막는다
-
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        static int number = 0;
 
         static void Thread_1()
         {
-            y = 1; // Store y
-
-            Thread.MemoryBarrier();
-
-            r1 = x; // Load x
+            for (int i = 0; i < number; i++)
+            {
+                Interlocked.Increment(ref number); // 1
+            }
         }
 
         static void Thread_2()
         {
-            x = 1;
-
-            Thread.MemoryBarrier();
-
-            r2 = y;
+            for (int i = 0; i < number; i++)
+            {
+                Interlocked.Decrement(ref number); // 0 
+            }
         }
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
+            Task.WaitAll(t1, t2);
 
-                Task.WaitAll(t1, t2);
-
-                if (r1 == 0 && r2 == 0)
-                {
-                    break;
-                }
-            }
-            Console.WriteLine($"{count}번만에 빠져나옴");
+            Console.WriteLine(number);
         }
     }
 }
