c0e5707dd7592e3ec0e268ad93bc91a4264f5f01
Timeisgold0622 <yjok9333@gmail.com>
2026-07-09 20:34:41 +0900

    Task 배움

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -12,6 +12,9 @@
         {
             ThreadPool.SetMinThreads(1, 1); // 최소 스레드 수
             ThreadPool.SetMaxThreads(5, 5); // 최대 스레드 수
+
+            Task t = new Task(() => {while (true) { } }, TaskCreationOptions.LongRunning); // LongRunning 인자로 인해서 먹통되지 않음.
+
             ThreadPool.QueueUserWorkItem(MainThread); // 필요할때 호출하는 Thread + 갯수 제한 있음
         }
     }
