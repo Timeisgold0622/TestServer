274eeeb85d1ac9a9a5b977185fe9c72693ad0798
Timeisgold0622 <yjok9333@gmail.com>
2026-07-09 20:09:25 +0900

    Thread 익히기 - 1

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace TestServer
 {
-    internal class Program
+    class Program
     {
+        static void MainThread()
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello, Thread!");
+            }
+        }
         static void Main(string[] args)
         {
+            Thread t = new Thread(MainThread);
+            t.Name = "Test Thread";
+            t.IsBackground = true;
+            t.Start();
+            Console.WriteLine("Wating for Thread!");
+            t.Join();
             Console.WriteLine("Hello, World!");
         }
     }
