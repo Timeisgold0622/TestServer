4d6f8ca76a5905f32014ffbf53bd82baa69e91dc
Timeisgold0622 <yjok9333@gmail.com>
2026-07-10 11:07:40 +0900

    ManualResetEvent

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -1,13 +1,14 @@
 ﻿namespace TestServer
 {
-    class SpinLock
+    class Lock
     {
         // bool <- 커널
-        AutoResetEvent _available = new AutoResetEvent(true);
+        ManualResetEvent _available = new ManualResetEvent(true);
         public void Acquire()
         {
             _available.WaitOne(); // 입장 시도
-            // _available.Reset(); // bool -> false (이부분이 WaitOne에 포함되어 있음)
+            _available.Reset(); // 자동으로 안해줘서 수동으로 닫아야됨
+            // 근데 이러면 연산이 두개로 분리되서 Race Condition이 나타남 그렇기 떄문에 ManualResetEvent는 여러 개의 스레드를 사용해야될때 쓴다고 볼 수 있음
         }
         public void Release()
         {
@@ -17,7 +18,7 @@
     class Program
     {
         static int _num = 0;
-        static SpinLock _lock = new SpinLock();
+        static Lock _lock = new Lock();
 
         static void Thread_1()
         {
