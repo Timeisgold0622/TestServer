d5a7e9c09a7352b32d893ed2681eb33a1db45b0f
Timeisgold0622 <yjok9333@gmail.com>
2026-07-10 10:18:07 +0900

    스핀락 잘못 구현된 버전

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -1,47 +1,57 @@
 ﻿namespace TestServer
 {
+    class SpinLock
+    {
+        volatile bool _locked = false; // 가시성
+        public void Acquire()
+        {
+            while (_locked)
+            {
+                // 잠김이 풀리기를 기다림
+            }
+
+            _locked = true;
+        }
+        public void Release()
+        {
+            _locked = false;
+        }
+    }
     class Program
     {
-        static int number = 0;
-        static object _obj = new object();
+        static int _num = 0;
+        static SpinLock _lock = new SpinLock();
 
         static void Thread_1()
         {
-            for (int i = 0; i < number; i++)
+            for (int i = 0; i < _num; i++)
             {
-
-                lock (_obj) // 아래랑 같은 코드인데 이제 잠금을 못푸는 경우를 방지해주는 구문
-                {
-                    number++;
-                }
-                // 상호 배체 Mutual Exclusive
-
-                // CriticalSection, std::mutex
-                Monitor.Enter(_obj); // 문을 잠구는 행위
-                number++;
-                Monitor.Exit(_obj); // 잠금을 풀어줌
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < number; i++)
+            for (int i = 0; i < _num; i++)
             {
-                Monitor.Enter(_obj);
-                number--;
-                Monitor.Exit(_obj);
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
             }
         }
         static void Main(string[] args)
         {
             Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+            Task t2 = new Task(Thread_1);
+
             t1.Start();
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(number);
+            Console.WriteLine(_num);
         }
     }
 }
