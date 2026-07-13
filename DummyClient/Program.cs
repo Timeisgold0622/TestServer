2c288472c3f9cf7d3d9ab50688edb4a42d0fb2c7
Timeisgold0622 <yjok9333@gmail.com>
2026-07-13 19:35:51 +0900

    매우 간단한 소켓 서버 구현

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace DummyClient
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 8080);
+
+            // 휴대폰 설정
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 문지기에게 요청 보내기
+                socket.Connect(endPoint); // 얘도 블로킹함수라, 문지기가 안 받아주면 대기함
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}"); //  socket.RemoteEndPoint은 연결한 대상임
+
+                // 보낸다.
+                byte[] sendbuff = Encoding.UTF8.GetBytes("Hello World!");
+                int sendBytes = socket.Send(sendbuff);
+
+                // 받는다.
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] : {recvData}");
+
+                // 나간다.
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
         }
     }
 }
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,32 +1,61 @@
-﻿namespace ServerCore
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
 {
     internal class Program
     {
-        // 스레드마다 고유한 자원 부여
-        // static ThreadLocal<string> ThreadName = new ThreadLocal<string>();
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => {return $"My Name is {Thread.CurrentThread.ManagedThreadId}";});
-        // 위 코드는 자동으로 ThreadName이 할당된 스레드면 새로 부여 안하고, 안되었으면 해당 return 값을 부여함
 
-
-        static void WhoAmI()
-        {
-            bool repeat = ThreadName.IsValueCreated; // 이 스레드가 이미 만들어졌으면 True, 아니면 False
-            if (repeat)
-            {
-                Console.WriteLine(ThreadName.Value + " (repeat)");
-            }
-            else
-            {
-                Console.WriteLine(ThreadName.Value); // 이때 위의 있는 new ThreadLocal<string>(() => {return $"My Name is {Thread.CurrentThread.ManagedThreadId}";})가 실행됨 
-            }
-        }
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(3, 3);
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            // DNS를 이용해 구현하겠음
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0]; // AddressList가 리스트인 이유가 서버 부하를 막기 위해 여러 주소를 사용할 수도 있기 때문인데,
+                                                      // 어짜피 우린 한 주소만 사용할 거라서 1번 주소를 쓸거
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 8080); // ip의 최종 주소임, 두번째 인자는 포트 값
+            // GetHostEntry를 통해 DNS 주소를 아이피 주소로 찾음
 
-            ThreadName.Dispose(); // 데이터 날리기
+            // 문지기 만들기
+            // AddressFamily는 ipv4 인지 ipv6를 사용할 것인지 설정하는 건데, 이미 DNS에서 설정되어 넣어져 있으니까 그냥 넣으면 됨
+            // 그리고 tcp 프로토콜을 사용할 경우 SocketType을 Stream과 ProtocolType을 Tcp로 설정해야됨
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 문지기 교육
+                listenSocket.Bind(endPoint); // ip 동기화라고 보면 됨
+
+                // 영업시간
+                // backlog : 최대 대기 수
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening....");
+
+                    // 손님을 입장시킨다
+                    Socket clientSocket = listenSocket.Accept(); // 여기서 반환되는 값이 대리인(세션)의 소켓임
+                    // 그리고 위 함수는 블로킹 함수라서, 위 함수가 실행이 원활히 되지 않으면 아랫단계 내용이 실행이 안됨
+                    // 받는다.
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvDatas = Encoding.UTF8.GetString(recvBuff, 0, recvBytes); // 데이터가 중간부터 데이터를 받을 수도 있지만 지금은 그런 경우가 없기 때문에 0부터 받음
+                    Console.WriteLine($"[from Client] : {recvDatas}");
+
+                    // 보낸다.
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to ERTServer");
+                    clientSocket.Send(sendBuff);
+
+                    // 쫒아낸다.
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close(); // 이거만 해도 되지만, 명시해주기 위해서 미리 셧다운을 호출해주기
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString();
+            }
         }
     }
 }
