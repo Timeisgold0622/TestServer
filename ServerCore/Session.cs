b7ef94636ec34c5b6aa39f1dc000e64bf2237b07
Timeisgold0622 <yjok9333@gmail.com>
2026-07-21 16:35:06 +0900

    Session 개선

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -14,19 +14,18 @@ namespace ServerCore
         Queue<byte[]> _sendQueue = new Queue<byte[]>();
         bool _pending = false;
         SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
-
+        SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
 
         public void Start(Socket socket)
         {
             _socket = socket;
-            SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
-            recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
-            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            _recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
             // recvArgs.UserToken 는 추가적으로 넣고 싶은 내용 있으면 넣는 거, object 형식이라 다 넣기 가능
             // 버퍼 만들기
-            recvArgs.SetBuffer(new byte[1024],0,1024);
+            _recvArgs.SetBuffer(new byte[1024],0,1024);
 
-            RegisterRecv(recvArgs);
+            RegisterRecv();
         }
 
         public void Send(byte[] sendBuff)
@@ -87,12 +86,12 @@ namespace ServerCore
                 }
             }
         }
-        void RegisterRecv(SocketAsyncEventArgs args)
+        void RegisterRecv()
         {
-            bool pending = _socket.ReceiveAsync(args);
+            bool pending = _socket.ReceiveAsync(_recvArgs);
             if (pending == false)
             {
-                OnRecvCompleted(null, args);
+                OnRecvCompleted(null, _recvArgs);
             }
         }
 
@@ -105,7 +104,7 @@ namespace ServerCore
                     string recvdata = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred); // Offset : 시작 위치, ByteTransferred : 몇 바이트 받았냐
                     Console.WriteLine($"[From Server] : {recvdata}");
 
-                    RegisterRecv(args);
+                    RegisterRecv();
                 }
                 catch (Exception e)
                 {
